a0a2a876fe857d406060c81f869312ff1645e441
naszar <naszar@ya.ru>
2014-06-24 16:58:42 +1100

    First commit. Charger controller for Sergey

---
+++ ser_charge/main.c
@@ -0,0 +1,66 @@
+#define F_CPU 1000000UL
+#include <avr/delay.h>
+#include <avr/io.h>
+
+#include "blink.h"
+#include "adc.h"
+
+#define V_TO_COUNTS(X)	(int) (X*256/5)
+#define V_BAT_MIN	V_TO_COUNTS(0.7)
+#define V_BAT_MAX	V_TO_COUNTS(4.3)
+#define TEMP_BAT_MIN	V_TO_COUNTS(0.1)
+#define TEMP_BAT_MAX	V_TO_COUNTS(2)
+#define CP_BAT_MIN	V_TO_COUNTS(0.1)
+#define CP_BAT_MAX	V_TO_COUNTS(1)
+
+#define V_BAT_PIN	0
+#define TEMP_BAT_PIN	1
+#define CP_BAT_PIN	2
+
+#define POWER_PIN	0
+#define POWER_PORT	PORTB
+#define POWER_CONTROL	DDRB
+
+main()
+{
+ uint8_t v_val;
+ uint8_t temp_val;
+ uint8_t cp_val;
+
+ lamp_init();
+ POWER_CONTROL |= 1 << POWER_PIN;
+ lamp_change_state(L_ALL_OFF);
+ 
+ while(1){
+	v_val = adc_get_value(V_BAT_PIN);
+	if( v_val > V_BAT_MIN){
+		temp_val = adc_get_value(TEMP_BAT_PIN);
+		if(temp_val > TEMP_BAT_MIN && temp_val < TEMP_BAT_MAX){
+			if(v_val < V_BAT_MAX){
+				POWER_PORT |= 1 << POWER_PIN;
+				cp_val = adc_get_value(CP_BAT_PIN);
+				if( cp_val > CP_BAT_MIN && cp_val < CP_BAT_MAX ){
+					lamp_change_state(L_GREEN_BLINK);
+					_delay_ms(500);
+					}
+				else
+					lamp_change_state(L_RED_BLINK);
+				POWER_PORT &= ~(1 << POWER_PIN);
+				}
+			else
+				lamp_change_state(L_GREEN_CONST);
+			}
+		else
+			if(v_val < V_BAT_MAX)
+				lamp_change_state(L_RED_BLINK);
+			else
+				lamp_change_state(L_GREEN_CONST);
+		}
+	else{
+		lamp_change_state(L_ALL_OFF);
+		_delay_ms(1000);
+		}
+	_delay_ms(500);	
+ };
+};
+ 	
